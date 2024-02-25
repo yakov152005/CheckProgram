6630b57b3b923511714f78382980090e1e0dd981
yakov152005 <155822770+yakov152005@users.noreply.github.com>
2024-02-25 13:10:12 +0200

    Add files via upload

---
+++ CheckProgram/src/Check.java
@@ -0,0 +1,48 @@
+import javax.swing.plaf.PanelUI;
+
+public class Check {
+	private String chNumber;
+	private String bName;
+	private int depNumber;
+	private float aMount;
+
+	//Constructor1
+	public Check(String chNumber, String bName, int depNumber, float aMount){
+		this.chNumber = chNumber;
+		this.bName = bName;
+		this.depNumber = depNumber;
+		this.aMount = aMount;
+	}
+	//Constructor2
+	public Check(){
+		this.chNumber = "UNKNOWN";
+		this.bName = "UNKNOWN";
+		this.depNumber =0;
+		this.aMount =0.0f;
+	}
+	public void setData(String chNumber, String bName, int depNumber, float aMount){
+		this.chNumber = chNumber;
+		this.bName = bName;
+		this.depNumber = depNumber;
+		this.aMount = aMount;
+	}
+	public String getChNumber(){
+		return chNumber;
+	}
+	public String getBName(){
+		return bName;
+	}
+	public int getDepNumber(){
+		return depNumber;
+	}
+	public float getaMount(){
+		return aMount;
+	}
+	public String toString(){
+		return "Bank's name: " + bName +
+				"\nCheck number: " +chNumber +
+				"\nBranch number: "  +depNumber +
+				"\nCheck amount: " + aMount;
+	}
+
+}
---
+++ CheckProgram/src/Program.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Check[] checkArr = new Check[3];
+		Check checkPtr = new Check("1212e", "National", 902, 1025.2f);
+		searchCheck(checkArr,checkPtr);
+	}
+	public static void searchCheck(Check[] checkArr, Check checkPtr){
+		creatCheckArr(checkArr);
+		for (int i = 0; i < checkArr.length; i++) {
+			if (checkPtr.getChNumber().equals(checkArr[i].getChNumber())){
+				System.out.println(checkArr[i].toString());
+			}else {
+				System.out.println("Check not exist");
+			}
+		}
+	}
+	public static Check[] creatCheckArr (Check[] checkArr){
+		String chNumber, bName;
+		int depNumber;
+		float aMount;
+		for (int i = 0; i < checkArr.length; i++) {
+			System.out.println("Enter details for Check |" + (i+1) + "|");
+			System.out.print("Check number: ");
+			chNumber = s.next();
+			System.out.print("Bank's name: ");
+			bName = s.next();
+			System.out.print("Branch number: ");
+			depNumber = s.nextInt();
+			System.out.print("Check amount: ");
+			aMount = s.nextFloat();
+
+			checkArr[i] = new Check(chNumber, bName, depNumber, aMount);
+		}
+		return checkArr;
+	}
+	public static Scanner s =new Scanner(System.in);
+}
